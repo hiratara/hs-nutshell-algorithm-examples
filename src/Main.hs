d0ac8664ed16ce3a39cd2d4acabf415b77ecf17e
Masahiro Honma <hiratara@cpan.org>
2018-09-08 16:11:59 +0900

    output slowhull.svg

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 import Control.Monad (guard)
+import Data.Default.Class (def)
 import qualified Data.List as List
 import Data.List ((\\), sortOn, uncons)
 import qualified Data.Set as Set
+import qualified Graphics.Rendering.Chart.Backend.Diagrams as Diagrams
+import qualified Graphics.Rendering.Chart.Easy as Chart
 import qualified System.Random.MWC as MWC
 
 data Point = Point !Double !Double deriving (Eq, Ord, Show)
@@ -53,4 +56,14 @@ main = do
   putStrLn "points generated"
   print ps
   putStrLn "convex hull"
-  print $ slowHull ps
+  let hull = slowHull ps
+  print hull
+
+  let pslist = map (\(Point x y) -> (x, y)) . Set.toList $ ps
+      hulllist = map (\(Point x y) -> (x, y)) hull
+      chart = Chart.toRenderable . Chart.execEC
+            $ do Chart.plot $ Chart.points "" pslist
+                 Chart.plot $ Chart.line "" [last hulllist : hulllist]
+  Diagrams.renderableToFile def "slowhull.svg" chart
+
+  return ()
